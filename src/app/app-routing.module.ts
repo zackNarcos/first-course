20ca7bd7eb99fd4dd99695821c6370573c9a2619
Zackarie Abessolo <58006990+zackNarcos@users.noreply.github.com>
2024-03-08 23:28:58 +0000

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {StudentListComponent} from "./student/student-list/student-list.component";
+import {StudentCreateComponent} from "./student/student-create/student-create.component";
+import {StudentDetailsComponent} from "./student/student-details/student-details.component";
+import {StudentEditComponent} from "./student/student-edit/student-edit.component";
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'students', component: StudentListComponent },
+  { path: 'students/create', component: StudentCreateComponent },
+  { path: 'students/:id', component: StudentDetailsComponent },
+  { path: 'students/:id/edit', component: StudentEditComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {StudentCreateComponent} from "./student/student-create/student-create.component";
+import {LoginComponent} from "./login/login.component";
+import {StudentDetailsComponent} from "./student/student-details/student-details.component";
+import {StudentEditComponent} from "./student/student-edit/student-edit.component";
+import {StudentListComponent} from "./student/student-list/student-list.component";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    StudentCreateComponent,
+    StudentDetailsComponent,
+    StudentEditComponent,
+    StudentListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
